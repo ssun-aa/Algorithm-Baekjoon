a0fb0a8b0e123393f28b17aeb40c9f48551258f4
ssunah <ssk6200@daum.net>
2021-05-04 11:48:08 +0900

    큐

--- boj10845.java
+++ boj10845.java
@@ -3,7 +3,6 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
