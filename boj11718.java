17734cc46f999c044fcdc90b91c323cb6110acba
ssunah <ssk6200@daum.net>
2021-06-01 20:24:46 +0900

    11718:그대로 출력하기

---
+++ boj11718.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class boj11718 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String st = sc.nextLine();
+            System.out.println(st);
+        }
+        sc.close();
+    }
+}
