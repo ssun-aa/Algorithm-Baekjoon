a5da6fe059d58e06623f80392c0eb36e6aadf2b7
ssunah <ssk6200@daum.net>
2021-07-22 00:03:30 +0900

    11726:2xn 타일링

--- boj11726.java
+++ boj11726.java
@@ -7,9 +7,9 @@ public class boj11726 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[1001];
+        int arr[] = new int[1001];// new int[n+1]으로 하면
         arr[1] = 1;
-        arr[2] = 2;
+        arr[2] = 2;//n=1일 경우 이 부분에서 런타임 에러가 발생
         for(int i=3; i<=n; i++){
             arr[i] = (arr[i-1] + arr[i-2]) % 10007;
         }
