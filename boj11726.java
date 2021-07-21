e750686fcbee360bc1da43ecd69ca78ec6e1a229
ssunah <ssk6200@daum.net>
2021-07-21 23:59:46 +0900

    11726:2xn 타일링

--- boj11726.java
+++ boj11726.java
@@ -7,12 +7,11 @@ public class boj11726 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[n+1];
+        int arr[] = new int[1001];
         arr[1] = 1;
         arr[2] = 2;
         for(int i=3; i<=n; i++){
             arr[i] = (arr[i-1] + arr[i-2]) % 10007;
-            System.out.println("!!");
         }
         System.out.println(arr[n]);
     }
