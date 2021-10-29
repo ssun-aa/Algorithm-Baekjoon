535c4090c19babc74386cb0136b616603bbc33ff
ssunah <ssk6200@daum.net>
2021-10-29 13:03:24 +0900

    11047:동전0

---
+++ boj11047.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        for(int i=n-1; i>=0; i--){
+            if(arr[i] <= k){
+                answer += (k / arr[i]);
+                k = k % arr[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
